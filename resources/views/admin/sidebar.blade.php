5f936999eb2dd95aee9e21c5bf439fd96016d471
AidynMakhataev <makataev.7@gmail.com>
2017-12-05 15:39:31 +0600

    handle crud for roles,permissions and users

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="col-md-3">
+    <div class="panel panel-default panel-flush">
+        <div class="panel-heading">
+            Sidebar
+        </div>
+
+        <div class="panel-body">
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                    <a href="{{ url('/admin') }}">
+                        Dashboard
+                    </a>
+                </li>
+                <li role="presentation">
+                    <a href="{{ url('/admin/role') }}">
+                        Role
+                    </a>
+                </li>
+                <li role="presentation">
+                    <a href="{{ url('/admin/permission') }}">
+                        Permission
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
