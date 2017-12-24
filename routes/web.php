b176bd5be5e1fdcb2ce8d26082f6aff5a81d89fe
AidynMakhataev <makataev.7@gmail.com>
2017-12-24 19:21:48 +0600

    remove trash from routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@
 Auth::routes();
 
 
+Route::get('/', 'FrontendController@home');
+
 Route::group(['middleware' => 'role:admin'], function () {
-    Route::redirect('/', '/admin/user');
     Route::redirect('/admin', 'admin/user');
     Route::resource('admin/permission', 'Admin\\PermissionController');
     Route::resource('admin/role', 'Admin\\RoleController');
@@ -13,11 +14,9 @@ Route::group(['middleware' => 'role:admin'], function () {
     Route::resource('admin/projects', 'Admin\\ProjectsController');
     Route::resource('admin/digital-reports', 'Admin\\DigitalReportsController');
     Route::resource('admin/smm-reports', 'Admin\\SmmReportsController');
+    Route::resource('admin/comments', 'Admin\\CommentsController');
 });
 
-Route::view('/instagram', 'home');
-
-Route::get('/', 'FrontendController@home');
 Route::middleware('role:user')->group(function () {
     Route::get('/digital', 'FrontendController@digital')->name('digital')->middleware('permission:view-digital-report');
     Route::get('/digital/period/{id}','FrontendController@getDigitalPeriod')->name('digitalPeriod')->middleware('permission:view-digital-report');
@@ -26,22 +25,3 @@ Route::middleware('role:user')->group(function () {
     Route::get('/smm', 'FrontendController@smm')->name('smm')->middleware('permission:view-smm-report');
     Route::get('/smm/period/{id}','FrontendController@getSmmPeriod')->name('smmPeriod')->middleware('permission:view-smm-report');
 });
-
-Route::get('/test', function () {
-    $data = [
-        'name' => 'Multistage Admin',
-        'email' => 'admin@multistage.kz',
-        'password' => bcrypt('mcW4eMzy')
-    ];
-    $user = App\User::create($data);
-    $user->assignRole('admin');
-
-    return $user;
-});
-
-
-
-
-
-
-Route::resource('admin/comments', 'Admin\\CommentsController');
